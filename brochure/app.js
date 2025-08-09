81479dfa6197e6d073578bba686c8f16fe349d00
ThanuraSilva <thanurasilva1989@gmail.com>
2025-08-09 14:40:39 +0530

    Apply business logic with implement  all form and click related tasks

--- brochure/app.js
+++ brochure/app.js
@@ -1,3 +1,26 @@
 const registerBtnClick = () =>{
     window.location.href = "form.html"
 }
+// Get form data 
+document.addEventListener("DOMContentLoaded", function(){
+// all other tasks
+const form = document.getElementById("formData");
+form.addEventListener("submit",function (e){
+   // tasks that handle when trigger submit event
+   e.preventDefault();
+   const formData = new FormData(form)
+   const data = Object.fromEntries(formData.entries())
+   console.log(data);
+   const jsonTypeDetails = JSON.stringify(data);
+   console.log(jsonTypeDetails)
+   form.reset();
+});
+
+});
+
+const formReset = ()=>{
+    document.getElementById("formData").reset();
+}
+const btnBack= ()=>{
+    window.location.href = "index.html"
+}
